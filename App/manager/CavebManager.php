3764eba10e432c33f04f4d48006e2654c443eef8
thomasop <tdss33@hotmail.com>
2023-04-12 11:17:45 +0200

    Add: controller add b and view

--- App/manager/CavebManager.php
+++ App/manager/CavebManager.php
@@ -6,6 +6,15 @@ use App\entity\Cavea;
 
 class CavebManager extends Manager
 {
+    public function all()
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare('SELECT id, nom, quantite, annee FROM cave_b');
+        $bdGetAll->execute();
+        $result = $bdGetAll->fetchAll(PDO::FETCH_ASSOC);
+        return $result;  
+    }
+
     public function allPosition()
     {
         $bd = $this->connection();
@@ -18,5 +27,124 @@ class CavebManager extends Manager
         $bdGetAll->execute();
         $result = $bdGetAll->fetchAll(PDO::FETCH_ASSOC);
         return $result;  
+    }
+
+    public function oneByPosition($colonne, $ligne)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare(
+            'SELECT c.*
+            FROM cave_b c
+            JOIN position_cave_b a ON c.id = a.cave_b_id
+            JOIN position_b p ON a.position_id = p.id
+            WHERE p.colonne = ?
+            AND p.ligne = ?'
+        );
+        $bdGetAll->execute(array($colonne, $ligne));
+        $result = $bdGetAll->fetch(PDO::FETCH_ASSOC);
+        return $result;  
+    }
+
+
+    public function getOne($slug, $annee)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare('SELECT * FROM cave_b WHERE slug = ? AND annee = ?');
+        $bdGetAll->execute([$slug, $annee]);
+        $result = $bdGetAll->fetch(PDO::FETCH_ASSOC);
+        return $result;  
+    }
+
+    public function getPos($colonne, $ligne)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare('SELECT id FROM position_b WHERE colonne = ? AND ligne = ?');
+        $bdGetAll->execute([$colonne, $ligne]);
+        $result = $bdGetAll->fetch(PDO::FETCH_ASSOC);
+        return $result;  
+    }
+
+    public function deletePosition($colonne, $ligne)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare(
+            'DELETE FROM position_b
+            WHERE position_b.colonne = ?
+            AND position_b.ligne = ?'
+        );
+        $bdGetAll->execute(array($colonne, $ligne));
+        return $bdGetAll;  
+    }
+
+    public function deleteBout($id)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare(
+            'DELETE FROM cave_b
+            WHERE cave_b.id = ?'
+        );
+        $bdGetAll->execute(array($id));
+        return $bdGetAll;  
+    }
+
+    public function addCave($nom, $appellation, $annee, $type, $region, $contenance, $slug, $pays)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare(
+            'INSERT INTO cave_b(nom, appellation, annee, typev, region, contenance, slug, pays) VALUES(?, ?, ?, ?, ?, ?, ?, ?)'
+        );
+        $bdGetAll->execute(array($nom, $appellation, $annee, $type, $region, $contenance, $slug, $pays));
+        return $bdGetAll;  
+    }
+
+    public function addPosition($colonne, $ligne)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare(
+            'INSERT INTO position_b(colonne, ligne) VALUES(?, ?)'
+        );
+        $bdGetAll->execute(array($colonne, $ligne));
+        return $bdGetAll;  
+    }
+
+    public function add($nom, $annee)
+    {
+        $bd = $this->connection();
+        $bdGetAll = $bd->prepare(
+            'INSERT INTO position_cave_b(position_id, cave_b_id) VALUES(?, ?)'
+        );
+        $bdGetAll->execute(array($nom, $annee));
+        return $bdGetAll;  
+    }
+
+    public function updateQtn($id, $qtn)
+    {
+        $bd = $this->connection();
+        $bdGet = $bd->prepare(
+            'UPDATE cave_b SET quantite = :qtn WHERE id = :id'
+        );
+        $bdGet->bindValue(':id', (int)$id["id"], PDO::PARAM_INT);
+        $bdGet->bindValue(':qtn', $qtn, PDO::PARAM_INT);
+        $bdGet->execute();
+        return $bdGet;  
+    }
+
+    public function update($id, $nom, $appellation, $annee, $typev, $region, $contenance, $slug, $pays)
+    {
+        $bd = $this->connection();
+        $bdGet = $bd->prepare(
+            'UPDATE cave_b SET nom = :nom, appellation = :appellation, annee = :annee, typev = :typev, region = :region, contenance = :contenance, slug = :slug, pays = :pays WHERE id = :id'
+        );
+        $bdGet->bindValue(':id', (int)$id["id"], PDO::PARAM_INT);
+        $bdGet->bindValue(':nom', $nom, PDO::PARAM_STMT);
+        $bdGet->bindValue(':appellation', $appellation, PDO::PARAM_STMT);
+        $bdGet->bindValue(':annee', $annee, PDO::PARAM_STMT);
+        $bdGet->bindValue(':typev', $typev, PDO::PARAM_STMT);
+        $bdGet->bindValue(':region', $region, PDO::PARAM_STMT);
+        $bdGet->bindValue(':contenance', $contenance, PDO::PARAM_STMT);
+        $bdGet->bindValue(':slug', $slug, PDO::PARAM_STMT);
+        $bdGet->bindValue(':pays', $pays, PDO::PARAM_STMT);
+        $bdGet->execute();
+        return $bdGet;  
     }
 }
