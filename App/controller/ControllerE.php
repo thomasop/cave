19d5781f60f77b0b0bf460401305ddeae945e4fd
thomasop <tdss33@hotmail.com>
2023-04-15 11:43:20 +0200

    Add: add e

--- App/controller/ControllerE.php
+++ App/controller/ControllerE.php
@@ -22,4 +22,37 @@ class ControllerE extends Controller
             'all' => $data[4],
         ]);
     }
+
+    public function addView()
+    {
+        $manager = new CaveeManager();
+        $caveArray = new CaveArray();
+        $data = $caveArray->array($manager);
+        $twigview = $this->getTwig();
+        $twigpostview = $twigview->load('cave_e/cave_e_add/index.twig');
+        echo $twigpostview->render([
+            'lists' => $data[0],
+            'letters' => $data[1],
+            'slug' => $data[3],
+            'all' => $data[4],
+        ]);
+    }
+
+    public function add()
+    {
+        $manager = new CaveeManager();
+        $manager->addCave(htmlspecialchars($_POST['nom']), htmlspecialchars($_POST['appellation']), htmlspecialchars($_POST['annee']), htmlspecialchars($_POST['type']), htmlspecialchars($_POST['region']), htmlspecialchars($_POST['contenance']), str_replace([" ", "â", "é", "è", "à", "ê", "î", "û", "ô"], ["-", "a", "e", "e", "a", "e", "i", "u", "o"], htmlspecialchars($_POST['nom'])), htmlspecialchars($_POST['pays']));
+        $one = $manager->getOne(str_replace([" ", "â", "é", "è", "à", "ê", "î", "û", "ô"], ["-", "a", "e", "e", "a", "e", "i", "u", "o"], htmlspecialchars($_POST['nom'])), htmlspecialchars($_POST['annee']));
+        $init = 0;
+        $caveArray = new CaveArray();
+        for ($i = 1; $i < 101; $i++) {
+            if (isset($_POST[(string) $i])) {
+                $loopFor = $caveArray->forLoop($_POST[(string) $i], $manager, $one, $init);
+                $init++;
+            }
+        }
+        $uuu = $manager->updateQtn($one, $init);
+        $this->phpSession()->set('stop', 'Une bouteille a été ajouté.');
+        $this->phpSession()->redirect('/cave2/e');
+    }
 }
