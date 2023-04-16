57f838da998a7789baee4fecb5f13bc3b3b4af11
thomasop <tdss33@hotmail.com>
2023-04-16 11:17:50 +0200

    Add: delete f

--- App/controller/ControllerF.php
+++ App/controller/ControllerF.php
@@ -113,4 +113,43 @@ class ControllerF extends Controller
             $this->phpSession()->redirect('/cave2/f');
         }
     }
+
+    public function deleteView()
+    {
+        $manager = new CavefManager();
+        $caveArray = new CaveArray();
+        $data = $caveArray->array($manager);
+        $twigview = $this->getTwig();
+        $twigpostview = $twigview->load('cave_f/cave_f_delete/index.twig');
+        echo $twigpostview->render([
+            'lists' => $data[0],
+            'letters' => $data[1],
+            'slug' => $data[3],
+            'all' => $data[4],
+        ]);
+    }
+
+    public function delete()
+    {
+        $manager = new CavefManager();
+        for ($i = 1; $i < 101; $i++) {
+            if (isset($_POST[$i])) {
+                $pos = $_POST[(string) $i];
+                if ($pos == true) {
+                    $split = explode("-", $pos);
+                    $getOne = $manager->oneByPosition($split[0], $split[1]);
+                    $manager->updateQtn($getOne, (string) ((int) $getOne["quantite"] - 1));
+                    $manager->deletePosition($split[0], $split[1]);
+                }
+            }
+        }
+        $caveAallB = $manager->all();
+        for ($i = 0; $i < count($caveAallB); $i++) {
+            if ($caveAallB[$i]["quantite"] == 0) {
+                $manager->deleteBout($caveAallB[$i]["id"]);
+            }
+        }
+        $this->phpSession()->set('stop', 'Une bouteille a été supprimé.');
+        $this->phpSession()->redirect('/cave2/f');
+    }
 }
